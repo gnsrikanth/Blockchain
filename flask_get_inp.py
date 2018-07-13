d509c8d1ef402c8f71c472b0f958e409f321ff88
Grandhi Naga Srikanth <nagasrikanthgrandhi@gmail.com>
2018-07-13 15:01:04 -0400

    Create flask_get_inp.py

---
+++ flask_get_inp.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def my_form():
+    return ('<form method="POST"><input name="text"><input type="submit"></form>')
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    processed_text = text.upper()
+    return processed_text
+app.run(host='0.0.0.0',port=1222)
