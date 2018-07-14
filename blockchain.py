536a5c0c1794b5318846c1eeb32ebbf6a22aa1da
Grandhi Naga Srikanth <nagasrikanthgrandhi@gmail.com>
2018-07-14 01:57:46 -0400

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -2,3 +2,9 @@ import datetime
 import hashlib
 import json
 from flask import Flaskmjsonify
+
+class Blockchain:
+   def __init__(self):
+      self.chain=[]
+      self.create_block(proof=1,prev_hash='0')
+      
