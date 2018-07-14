409fb3c4b318c29a99e385aa66eae79cfa9f6274
Grandhi Naga Srikanth <nagasrikanthgrandhi@gmail.com>
2018-07-14 02:23:21 -0400

    Update blockchain.py

--- project/blockchain.py
+++ project/blockchain.py
@@ -2,3 +2,13 @@ import datetime
 import hashlib
 import json
 from flask import Flask, jsonify
+class Blockchain:
+   def __init__(self):
+      self.chain=[]
+      self.create.block(proof=0,previous_hash='0',data)
+   def create_block(self,proof,previous_hash,data):
+      block={'index':len(self.chain) + 1,
+            'timestamp':str(datetime.datetime.now()),
+            'proof':proof,
+            'previous_hash':previous_hash,
+            'data':data}
