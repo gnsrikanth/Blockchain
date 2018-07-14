0aff66fc50660af294785e76af1d10972ee26969
Grandhi Naga Srikanth <nagasrikanthgrandhi@gmail.com>
2018-07-14 06:51:29 -0400

    Create save_rsa_keys.py

---
+++ crypt/save_rsa_keys.py
@@ -0,0 +1,11 @@
+from Crypto.PublicKey import RSA
+private_key = RSA.generate(1024)
+public_key = private_key.publickey()
+print(private_key.exportKey(format='PEM'))
+print(public_key.exportKey(format='PEM'))
+
+with open ("private.pem", "w") as prv_file:
+    print("{}".format(private_key.exportKey()), file=prv_file)
+
+with open ("public.pem", "w") as pub_file:
+    print("{}".format(public_key.exportKey()), file=pub_file)
