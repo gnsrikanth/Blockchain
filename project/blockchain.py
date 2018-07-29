f3e51458ebe786d9fdc0ce5c2f708347d432ac42
Grandhi Naga Srikanth <nagasrikanthgrandhi@gmail.com>
2018-07-29 00:25:00 -0400

    Update blockchain.py

--- project/blockchain.py
+++ project/blockchain.py
@@ -11,16 +11,20 @@ class crypt:
 
   def hashdata(data):
     return hash.new(data.encode()).digest()
+  def sign(privatekey,data):
+		datahash=hash.new(data.encode()).digest()
+		return privatekey.sign(datahash,'')
 
 class Blockchain:
   def __init__():
-      chain=[]
-  
-
-  
+      chain=[]  
+      
   def create_block(data):
     new_block={'id':str(len(chain)+1),
               'address':str(crypt.b64(public_key)),
               'time':str(time.time()),
               'data'str(data),
-              'myblockhash'=str(crypt.hash())}
+              'myblockhash'=str(crypt.hash(((str(len(chain)+1)+(str(crypt.b64(public_key))+(str(time.time())+(str(data))))).encode())),
+              'sign'=crypt.sign(privatekey,str(crypt.hash(((str(len(chain)+1)+(str(crypt.b64(public_key))+(str(time.time())+(str(data))))).encode())))
+                                }
+    return new_block
