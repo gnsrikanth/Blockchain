01709b4cb30c8f199e5b27af329d3354316251bf
Grandhi Naga Srikanth <nagasrikanthgrandhi@gmail.com>
2018-07-29 00:12:02 -0400

    Update blockchain.py

--- project/blockchain.py
+++ project/blockchain.py
@@ -1,6 +1,20 @@
-import datetime
-import hashlib
-import json
-from flask import Flask, jsonify
+import time
+import Crypto
+from Crypto.PublicKey import RSA
+from Crypto import Random
+from Crypto.Hash import SHA256 as hash
+import base64
 
 class Blockchain:
+  def __init__():
+      chain=[]
+  
+  def b64(data):
+    b64=base64.b64encode(data)
+    return b64
+  
+  def create_block(data):
+    new_block={'id':str(len(chain)+1),
+              'address':b64(public_key),
+              'time':str(time.time()),
+              ''}
