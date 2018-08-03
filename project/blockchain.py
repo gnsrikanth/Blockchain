03c3a02bef3dbdad2a0a37e54c7a4121888cf319
Grandhi Naga Srikanth <nagasrikanthgrandhi@gmail.com>
2018-08-03 00:52:56 -0400

    Update blockchain.py

--- project/blockchain.py
+++ project/blockchain.py
@@ -5,6 +5,8 @@ from Crypto.Hash import SHA256 as hash
 import base64
 import time
 
+block_chain=[]
+
 class rsacrypt:
 	def gen():
 		length=1024
@@ -19,9 +21,27 @@ class rsacrypt:
 		retuen privatekey.sign(data,'')
 	def verify(publickey,data,signature):
 		return publickey.verify(data,signature)
+
 class crypt:
 	def hashthis(data):
 		if type(data)=="bytes":
 			return hash.new(data).hexdigest()
 		else:
 			return hash.new(data.encode()).hexdigest()
+	def b64en(data):
+        	return base64.b64encode(data)
+	def b64de(data):
+		retuen base64.b64decode(data)
+
+class blockchain:
+	def create_block(data):
+		bid=str(len(block_chain)+1)
+		timestamp=str(time.time.now)
+		data=str(data)
+		publickey=str((crypt.b64en(my_publickey.exportKey('PEM'))).decode())
+		previous_block_hash=str(crypt.hashthis(str(block_chain[-1])))
+		hashblock=str(crypt.hashthis(bid+timestamp+data+publickey+previous_block_hash))
+		sign=rsacrypt.sign(my_privatekey,hashblock)
+		nweblock={'id'=bid,
+			 'timestamp'=timestamp,
+			 }
