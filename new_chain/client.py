539d20145c86234f2aacd5b080ea87083d34519b
Grandhi Naga Srikanth <nagasrikanthgrandhi@gmail.com>
2018-09-01 00:52:35 -0400

    Update client.py

--- new_chain/client.py
+++ new_chain/client.py
@@ -11,10 +11,14 @@ class rsacrypt:
 		privatekey=RSA.generate(length,Random.new().read)
 		publickey=privatekey.publickey()
 		return privatekey,publickey
-	#def encrypt():
-		
-	#def decrypt():
-		
+	def encrypt(rsa_publickey,plain_text):
+		cipher_text=rsa_publickey.encrypt(plain_text,32)[0]
+		b64cipher=base64.b64encode(cipher_text)
+		return b64cipher
+	def decrypt(b64cipher, rsa_privatekey):
+		decoded_ciphertext = base64.b64decode(b64cipher)
+		plaintext = rsa_privatekey.decrypt(decoded_ciphertext)
+		return plaintext
 	def sign(privatekey,data):
 		if type(data)!=str:
 			return privatekey.sign(data,'')
@@ -40,7 +44,7 @@ class crypt:
 #        CLIENT     		#
 #########################
 c=rsacrypt()
-Aprivate,Apublic=c.gen()
+private,public=c.gen()
 import socket
 import requests
 
@@ -51,4 +55,15 @@ s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
 s.connect(ip,port)
 print('[+]Connected! ',s)
 
+#### get chain
+def getchain(ip,port):
+	chain=requests.get(f'http://{ip}:{port}/get_chain')
+	return (chain.text)
+
 #STEP 1 GENERATE RANDOM STR
+def step1(rand_string,block,recv_block):
+	public_recv=
+	sign=rsacrypt.sign(private,rsacrypt.encrypt(rand_string,public_recv))
+	#publickeystr=(privatekey.exportKey('PEM')).decode()
+	hash_string=crypt.hash(rand_string+str(block)+sign)
+	
