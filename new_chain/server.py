5e6a9b8e6b18cb1c51c7a6e9250c7f8936a549e6
Grandhi Naga Srikanth <nagasrikanthgrandhi@gmail.com>
2018-09-01 00:30:43 -0400

    Create server.py

---
+++ new_chain/server.py
@@ -0,0 +1,54 @@
+import Crypto
+from Crypto.PublicKey import RSA
+from Crypto import Random
+from Crypto.Hash import SHA256 as hash
+import base64
+import time
+
+class rsacrypt:
+	def gen():
+		length=1024
+		privatekey=RSA.generate(length,Random.new().read)
+		publickey=privatekey.publickey()
+		return privatekey,publickey
+	#def encrypt():
+		
+	#def decrypt():
+		
+	def sign(privatekey,data):
+		if type(data)!=str:
+			return privatekey.sign(data,'')
+		else:
+			return privatekey.sign(data.encode(),'')
+	def verify(publickey,data,signature):
+		return publickey.verify(data,signature)
+
+class crypt:
+	def hashthis(data):
+		if type(data)!=str:
+			return hash.new(data).hexdigest()
+		else:
+			return hash.new(data.encode()).hexdigest()
+	def b64en(data):
+		if type(data)!=str:
+			return base64.b64encode(data)
+		else:
+			return base64.b64encode(data.encode())
+	def b64de(data):
+		return base64.b64decode(data)
+#########################
+#        SERVER		#
+#########################
+c=rsacrypt()
+Aprivate,Apublic=c.gen()
+import socket
+import requests
+
+ip="0.0.0.0"
+port=8080
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+s.bind((ip,port))
+s.listen(1)
+conn,addr=s.accept()
+print('[+]Connected to',addr)
